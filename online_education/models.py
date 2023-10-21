8c43ec9e6144f811a97420cc88d278747240913d
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-10-21 20:20:21 +0300

    add models and serializers

--- online_education/models.py
+++ online_education/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+from users.models import NULLABLE
+
+
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название курса')
+    preview = models.ImageField(upload_to='Course/', verbose_name='превью курса', **NULLABLE)
+    description = models.TextField(max_length=1000, verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название урока')
+    description = models.TextField(max_length=1000, verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(upload_to='Lesson/', verbose_name='превью урока', **NULLABLE)
+    video_link = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+
