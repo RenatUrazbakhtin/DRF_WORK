d4eb2e06b484738855fe5939a9de9068af7d59b4
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-10-23 21:25:52 +0300

    changes to create user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from users.serializers import UserCreateSerializer
+
 
 # Create your views here.
+class UserCreateAPIView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
