dd3724cacefe21bb8c69e6ac54496b649172aedb
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-10-25 00:42:12 +0300

    url for permissions

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,15 @@ from django.urls import path
 from users.apps import UsersConfig
 from users.views import UserCreateAPIView
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('user/create/', UserCreateAPIView.as_view(), name='user-create'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
